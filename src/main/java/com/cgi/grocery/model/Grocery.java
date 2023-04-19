f219c34022b97b476a2896c8e2dfbf6939473118
sreenivasulu <sreenivasulu.sannibo@NL-78M9M63.groupinfra.com>
2023-04-19 11:14:41 +0200

    adding jenkinfile

--- src/main/java/com/cgi/grocery/model/Grocery.java
+++ src/main/java/com/cgi/grocery/model/Grocery.java
@@ -2,13 +2,35 @@ package com.cgi.grocery.model;
 
 import org.springframework.stereotype.Component;
 
-import lombok.Data;
 
-@Data
 @Component
 public class Grocery {
 
 	private String itemName;
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
 
 	private float price;
 
